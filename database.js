ad5e62fc57c6dba34f1c2e0e5b8d68ce0bfbe80a
shruti <shruti8barahate@gmail.com>
2023-09-22 12:17:53 +0530

    a

--- database.js
+++ database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-      const conn = await mongoose.connect('mongodb+srv://twinkle88bara:twinkle88@cluster0.ngrxjbv.mongodb.net/blogapp', {
+      const conn = await mongoose.connect('mongodb+srv://twinkle88bara:shruti12345@cluster0.ngrxjbv.mongodb.net/blogapp', {
         useNewUrlParser: true,
       });
       console.log(`MongoDB Connected: ${conn.connection.host}`);
