af397de92b402a2334f8bdbe32d51d7a3ed3b1a0
shruti0802 <137978020+shrutibara@users.noreply.github.com>
2023-09-25 17:35:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,65 +5,118 @@ const Post = require('./models/postmodel');
 const Comment = require('./models/commentmodel');
 // const bcrypt = require('bcrypt');
 
-const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
+
+
+
+
+
+  const mongoose = require('mongoose');
+
+ const bodyparser=require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 4000;
-const cors = require('cors');
-app.use(cors());
-app.use(bodyparser.json())
 
+const PORT = process.env.PORT || 4000;
+ const cors = require('cors');
+ app.use(cors());
+ app.use(bodyparser.json())
+ 
 connectDB()
-app.post('/users/register', async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
-    console.log(username, email, password);
+const crypto = require('crypto');
+var userProfile;
+
+const sessionSecret = crypto.randomBytes(32).toString('hex');
+app.use(session({
+  secret: sessionSecret, 
+  resave: false,
+  saveUninitialized: true,
+}));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+    clientID: '209115374976-huetf1qve753h4rqrot348gcgih8ok2v.apps.googleusercontent.com/',
+    clientSecret: "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2",
+    callbackURL: "https://backnode-becd.onrender.com/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    userProfile=profile;
+    return done(null, userProfile);
+}
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+app.post('/users/register',async (req,res)=>{
+  try{
+    const {username,email,password}=req.body;
     // const hashedPassword = await bcrypt.hash(password, 10);
-    const user = new User({ username, email, password, role: "USER" });
-    console.log(user);
+    const user = new User({ username,email, password,role : "USER" });
     await user.save();
     res.json(user)
-
-  } catch {
+  }catch{
     res.status(500).json({ error: 'Could not fetch user' });
   }
 });
-app.post('/api/login', async (req, res) => {
-  const { username, password } = req.body;
-  console.log(username, password);
-  const user = await User.findOne({ username, password });
-  console.log(user);
-  if (user) {
+app.post('/api/login',async (req,res)=>{
+  const{username,password}=req.body;
+  console.log(username,password);
+  const user = await User.findOne({username,password});
+  // console.log(user);
+  if(user){
     res.status(200).json(user);
-  } else {
-    res.status(401).json({ error: 'Invalid username or password' });
+  }else{
+    res.status(401).json({error:'Invalid username or password'});
   }
 });
 app.post('/api/posts', async (req, res) => {
   try {
+   
     const { title, description, author } = req.body;
-    const newPost = new Post({ title, description, author });
+    console.log(author);
+   
+    
+    const newPost = new Post({ title, description, author }); 
+    
+   ;
     await newPost.save();
     const user = await User.findById(author);
     if (user) {
-      user.posts.push(newPost._id);
+      user.posts.push(newPost.username);
       await user.save();
     }
+   
+
     res.status(201).json({ message: 'Post saved successfully' });
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Failed to save post' });
   }
 });
-app.get("/api/posts", async (req, res) => {
+app.get('/api/posts', async (req, res) => {
   try {
+    
+   
     const userId = req.query.userId;
-    const posts = await Post.find({ author: userId }); // Retrieve all posts from the collection
-    res.json(posts);
+    console.log(userId);
+
+    const posts = await Post.find({ author: userId });
+    console.log(posts);
+
+
+    res.status(200).json(posts);
   } catch (error) {
-    console.error("Error:", error);
-    res.status(500).json({ error: "Failed to fetch posts" });
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Failed to fetch user posts' });
   }
 });
 app.post('/api/posts/:postId/like', async (req, res) => {
@@ -75,7 +128,7 @@ app.post('/api/posts/:postId/like', async (req, res) => {
     if (!post) {
       return res.status(404).json({ message: 'Blog post not found' });
     }
-
+    
     post.likes += 1;
     await post.save();
 
@@ -87,7 +140,22 @@ app.post('/api/posts/:postId/like', async (req, res) => {
 });
 
 
+  app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+
+  // app.get(
+  //   '/auth/google/callback',
+  //   passport.authenticate('google', {
+  //     successRedirect: '/MainPage', 
+  //     failureRedirect: '/error', 
+  //   })
+  // );
+  app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/error' }),
+  function(req, res) {
+    // Successful authentication, redirect success.
+    res.send(userProfile);
+  });
 
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
-// app.listen(PORT,() => console.log(`Server running on port ${PORT}`))
+
+  app.listen(PORT,() => console.log(`Server running on port ${PORT}`))
